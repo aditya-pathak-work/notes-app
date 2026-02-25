0f71183e61739b003b46023b018b5bc55aa214e7
AP <pathakaditya2014.work@gmail.com>
2026-02-25 11:24:34 +0530

    update: added Notes.tsx page component

---
+++ resources/js/pages/Notes.tsx
@@ -0,0 +1,10 @@
+import { Head } from "@inertiajs/react";
+
+export default function Notes() {
+    return (
+        <div>
+            <Head title="Notes" />
+            <h1>Notes</h1>
+        </div>
+    );
+}
