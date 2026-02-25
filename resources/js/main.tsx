61b7742ffbcdf924167fbc1ef920042d51e32411
AP <pathakaditya2014.work@gmail.com>
2026-02-25 11:23:10 +0530

    update: added typescript support to frontend, renamed main.jsx to main.tsx

---
+++ resources/js/main.tsx
@@ -0,0 +1,22 @@
+import { createInertiaApp } from '@inertiajs/react'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client'
+
+createInertiaApp({
+    resolve: (name: string) => {
+        const pages = import.meta.glob('./pages/**/*.tsx', { eager: true })
+        return pages[`./pages/${name}.tsx`]
+    },
+    setup({ el, App, props }) {
+        createRoot(el).render(
+            <StrictMode>
+                <App {...props} />
+            </StrictMode>
+        )
+    },
+    defaults: {
+        future: {
+            useScriptElementForInitialPage: true,
+        },
+    },
+});
