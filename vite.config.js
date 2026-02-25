a05d1b8da72c3eccf78c0ec2b82a619a84910573
AP <pathakaditya2014.work@gmail.com>
2026-02-25 11:18:31 +0530

    update: added vite.config.js, configured for laravel and inertia

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react-swc';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/js/main.tsx'],
+            refresh: true,
+        }),
+        tailwindcss(),
+        react(),
+    ],
+    server: {
+        watch: {
+            ignored: ['**/storage/framework/views/**'],
+        },
+    },
+});
